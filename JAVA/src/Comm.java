891d401ed422eff01a597fc8226b5df7171b5876
jdoutreloux <justine.doutreloux@hotmail.be>
2018-05-14 13:49:33 +0200

    retour en arrière

--- JAVA/src/Comm.java
+++ JAVA/src/Comm.java
@@ -10,240 +10,240 @@ import java.util.TooManyListenersException;
 
 public class Comm implements SerialPortEventListener
 {
-    //passed from main GUI
-    GUI window = null;
+	//passed from main GUI
+	GUI window = null;
 
-    //for containing the ports that will be found
-    private Enumeration ports = null;
-    //map the port names to CommPortIdentifiers
-    private HashMap portMap = new HashMap();
+	//for containing the ports that will be found
+	private Enumeration ports = null;
+	//map the port names to CommPortIdentifiers
+	private HashMap portMap = new HashMap();
 
-    //this is the object that contains the opened port
-    private CommPortIdentifier selectedPortIdentifier = null;
-    private SerialPort serialPort = null;
+	//this is the object that contains the opened port
+	private CommPortIdentifier selectedPortIdentifier = null;
+	private SerialPort serialPort = null;
 
-    //input and output streams for sending and receiving data
-    private InputStream input = null;
-    private OutputStream output = null;
+	//input and output streams for sending and receiving data
+	private InputStream input = null;
+	private OutputStream output = null;
 
-    //just a boolean flag that i use for enabling
-    //and disabling buttons depending on whether the program
-    //is connected to a serial port or not
-    private boolean bConnected = false;
+	//just a boolean flag that i use for enabling
+	//and disabling buttons depending on whether the program
+	//is connected to a serial port or not
+	private boolean bConnected = false;
 
-    //the timeout value for connecting with the port
-    final static int TIMEOUT = 2000;
+	//the timeout value for connecting with the port
+	final static int TIMEOUT = 2000;
 
-    //some ascii values for for certain things
-    final static int CARRIAGE_RETURN_ASCII = 13;
-    final static int SPACE_ASCII = 32;
-    final static int DASH_ASCII = 45;
-    final static int NEW_LINE_ASCII = 10;
+	//some ascii values for for certain things
+	final static int CARRIAGE_RETURN_ASCII = 13;
+	final static int SPACE_ASCII = 32;
+	final static int DASH_ASCII = 45;
+	final static int NEW_LINE_ASCII = 10;
 
-    //a string for recording what goes on in the program
-    //this string is written to the GUI
-    String logText = "";
+	//a string for recording what goes on in the program
+	//this string is written to the GUI
+	String logText = "";
 
-    public Comm(GUI window)
-    {
-        this.window = window;
-    }
+	public Comm(GUI window)
+	{
+		this.window = window;
+	}
 
-    //search for all the serial ports
-    //pre: none
-    //post: adds all the found ports to a combo box on the GUI
-    public void searchForPorts()
-    {
-        ports = CommPortIdentifier.getPortIdentifiers();
+	//search for all the serial ports
+	//pre: none
+	//post: adds all the found ports to a combo box on the GUI
+	public void searchForPorts()
+	{
+		ports = CommPortIdentifier.getPortIdentifiers();
 
-        while (ports.hasMoreElements())
-        {
-            CommPortIdentifier curPort = (CommPortIdentifier)ports.nextElement();
+		while (ports.hasMoreElements())
+		{
+			CommPortIdentifier curPort = (CommPortIdentifier)ports.nextElement();
 
-            //get only serial ports
-            if (curPort.getPortType() == CommPortIdentifier.PORT_SERIAL)
-            {
-                window.cboxPorts.addItem(curPort.getName());
-                portMap.put(curPort.getName(), curPort);
-            }
-        }
-    }
+			//get only serial ports
+			if (curPort.getPortType() == CommPortIdentifier.PORT_SERIAL)
+			{
+				window.cboxPorts.addItem(curPort.getName());
+				portMap.put(curPort.getName(), curPort);
+			}
+		}
+	}
 
-    //connect to the selected port in the combo box
-    //pre: ports are already found by using the searchForPorts method
-    //post: the connected comm port is stored in commPort, otherwise,
-    //an exception is generated
-    public void connect()
-    {
-        String selectedPort = (String)window.cboxPorts.getSelectedItem();
-        selectedPortIdentifier = (CommPortIdentifier)portMap.get(selectedPort);
+	//connect to the selected port in the combo box
+	//pre: ports are already found by using the searchForPorts method
+	//post: the connected comm port is stored in commPort, otherwise,
+	//an exception is generated
+	public void connect()
+	{
+		String selectedPort = (String)window.cboxPorts.getSelectedItem();
+		selectedPortIdentifier = (CommPortIdentifier)portMap.get(selectedPort);
 
-        CommPort commPort = null;
+		CommPort commPort = null;
 
-        try
-        {
-            //the method below returns an object of type CommPort
-            commPort = selectedPortIdentifier.open("TigerControlPanel", TIMEOUT);
-            //the CommPort object can be casted to a SerialPort object
-            serialPort = (SerialPort)commPort;
+		try
+		{
+			//the method below returns an object of type CommPort
+			commPort = selectedPortIdentifier.open("TigerControlPanel", TIMEOUT);
+			//the CommPort object can be casted to a SerialPort object
+			serialPort = (SerialPort)commPort;
 
-            //for controlling GUI elements
-            setConnected(true);
+			//for controlling GUI elements
+			setConnected(true);
 
-            //logging
-            logText = selectedPort + " opened successfully.";
-            window.txtLog.setForeground(Color.black);
-            window.txtLog.append(logText + "\n");
+			//logging
+			logText = selectedPort + " opened successfully.";
+			window.txtLog.setForeground(Color.black);
+			window.txtLog.append(logText + "\n");
 
-            //CODE ON SETTING BAUD RATE ETC OMITTED
-            //XBEE PAIR ASSUMED TO HAVE SAME SETTINGS ALREADY
+			//CODE ON SETTING BAUD RATE ETC OMITTED
+			//XBEE PAIR ASSUMED TO HAVE SAME SETTINGS ALREADY
 
-            //enables the controls on the GUI if a successful connection is made
-            window.keybindingController.toggleControls();
-        }
-        catch (PortInUseException e)
-        {
-            logText = selectedPort + " is in use. (" + e.toString() + ")";
-            
-            window.txtLog.setForeground(Color.RED);
-            window.txtLog.append(logText + "\n");
-        }
-        catch (Exception e)
-        {
-            logText = "Failed to open " + selectedPort + "(" + e.toString() + ")";
-            window.txtLog.append(logText + "\n");
-            window.txtLog.setForeground(Color.RED);
-        }
-    }
+			//enables the controls on the GUI if a successful connection is made
+			window.keybindingController.toggleControls();
+		}
+		catch (PortInUseException e)
+		{
+			logText = selectedPort + " is in use. (" + e.toString() + ")";
 
-    //open the input and output streams
-    //pre: an open port
-    //post: initialized intput and output streams for use to communicate data
-    public boolean initIOStream()
-    {
-        //return value for whether opening the streams is successful or not
-        boolean successful = false;
+			window.txtLog.setForeground(Color.RED);
+			window.txtLog.append(logText + "\n");
+		}
+		catch (Exception e)
+		{
+			logText = "Failed to open " + selectedPort + "(" + e.toString() + ")";
+			window.txtLog.append(logText + "\n");
+			window.txtLog.setForeground(Color.RED);
+		}
+	}
 
-        try {
-            //
-            input = serialPort.getInputStream();
-            output = serialPort.getOutputStream();
-            writeData(0);
-            
-            successful = true;
-            return successful;
-        }
-        catch (IOException e) {
-            logText = "I/O Streams failed to open. (" + e.toString() + ")";
-            window.txtLog.setForeground(Color.red);
-            window.txtLog.append(logText + "\n");
-            return successful;
-        }
-    }
+	//open the input and output streams
+	//pre: an open port
+	//post: initialized intput and output streams for use to communicate data
+	public boolean initIOStream()
+	{
+		//return value for whether opening the streams is successful or not
+		boolean successful = false;
 
-    //starts the event listener that knows whenever data is available to be read
-    //pre: an open serial port
-    //post: an event listener for the serial port that knows when data is received
-    public void initListener()
-    {
-        try
-        {
-            serialPort.addEventListener(this);
-            serialPort.notifyOnDataAvailable(true);
-        }
-        catch (TooManyListenersException e)
-        {
-            logText = "Too many listeners. (" + e.toString() + ")";
-            window.txtLog.setForeground(Color.red);
-            window.txtLog.append(logText + "\n");
-        }
-    }
+		try {
+			//
+			input = serialPort.getInputStream();
+			output = serialPort.getOutputStream();
+			writeData(0);
 
-    //disconnect the serial port
-    //pre: an open serial port
-    //post: clsoed serial port
-    public void disconnect()
-    {
-        //close the serial port
-        try
-        {
-            writeData(0);
+			successful = true;
+			return successful;
+		}
+		catch (IOException e) {
+			logText = "I/O Streams failed to open. (" + e.toString() + ")";
+			window.txtLog.setForeground(Color.red);
+			window.txtLog.append(logText + "\n");
+			return successful;
+		}
+	}
 
-            serialPort.removeEventListener();
-            serialPort.close();
-            input.close();
-            output.close();
-            setConnected(false);
-            window.keybindingController.toggleControls();
+	//starts the event listener that knows whenever data is available to be read
+	//pre: an open serial port
+	//post: an event listener for the serial port that knows when data is received
+	public void initListener()
+	{
+		try
+		{
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+		}
+		catch (TooManyListenersException e)
+		{
+			logText = "Too many listeners. (" + e.toString() + ")";
+			window.txtLog.setForeground(Color.red);
+			window.txtLog.append(logText + "\n");
+		}
+	}
 
-            logText = "Disconnected.";
-            window.txtLog.setForeground(Color.red);
-            window.txtLog.append(logText + "\n");
-        }
-        catch (Exception e)
-        {
-            logText = "Failed to close " + serialPort.getName() + "(" + e.toString() + ")";
-            window.txtLog.setForeground(Color.red);
-            window.txtLog.append(logText + "\n");
-        }
-    }
+	//disconnect the serial port
+	//pre: an open serial port
+	//post: clsoed serial port
+	public void disconnect()
+	{
+		//close the serial port
+		try
+		{
+			writeData(0);
 
-    final public boolean getConnected()
-    {
-        return bConnected;
-    }
+			serialPort.removeEventListener();
+			serialPort.close();
+			input.close();
+			output.close();
+			setConnected(false);
+			window.keybindingController.toggleControls();
 
-    public void setConnected(boolean bConnected)
-    {
-        this.bConnected = bConnected;
-    }
+			logText = "Disconnected.";
+			window.txtLog.setForeground(Color.red);
+			window.txtLog.append(logText + "\n");
+		}
+		catch (Exception e)
+		{
+			logText = "Failed to close " + serialPort.getName() + "(" + e.toString() + ")";
+			window.txtLog.setForeground(Color.red);
+			window.txtLog.append(logText + "\n");
+		}
+	}
 
-    //what happens when data is received
-    //pre: serial event is triggered
-    //post: processing on the data it reads
-    public void serialEvent(SerialPortEvent evt) {
-        if (evt.getEventType() == SerialPortEvent.DATA_AVAILABLE)
-        {
-            try
-            {
-                byte singleData = (byte)input.read();
+	final public boolean getConnected()
+	{
+		return bConnected;
+	}
 
-                if (singleData != NEW_LINE_ASCII)
-                {
-                    logText = new String(new byte[] {singleData});
-                    window.txtLog.append(logText);
-                }
-                else
-                {
-                    window.txtLog.append("\n");
-                }
-            }
-            catch (Exception e)
-            {
-                logText = "Failed to read data. (" + e.toString() + ")";
-                window.txtLog.setForeground(Color.red);
-                window.txtLog.append(logText + "\n");
-            }
-        }
-    }
+	public void setConnected(boolean bConnected)
+	{
+		this.bConnected = bConnected;
+	}
 
-    //method that can be called to send data
-    //pre: open serial port
-    //post: data sent to the other device
-    public void writeData(int data)
-    {
-    		String s = Integer.toString(data);
-        try
-        {
-            output.write(s.getBytes());
-            output.write(CARRIAGE_RETURN_ASCII);
-            output.flush();
-        }
-        catch (Exception e)
-        {
-            logText = "Failed to write data. (" + e.toString() + ")";
-            window.txtLog.setForeground(Color.red);
-            window.txtLog.append(logText + "\n");
-        }
-    }
+	//what happens when data is received
+	//pre: serial event is triggered
+	//post: processing on the data it reads
+	public void serialEvent(SerialPortEvent evt) {
+		if (evt.getEventType() == SerialPortEvent.DATA_AVAILABLE)
+		{
+			try
+			{
+				byte singleData = (byte)input.read();
+
+				if (singleData != NEW_LINE_ASCII)
+				{
+					logText = new String(new byte[] {singleData});
+					window.txtLog.append(logText);
+				}
+				else
+				{
+					window.txtLog.append("\n");
+				}
+			}
+			catch (Exception e)
+			{
+				logText = "Failed to read data. (" + e.toString() + ")";
+				window.txtLog.setForeground(Color.red);
+				window.txtLog.append(logText + "\n");
+			}
+		}
+	}
+
+	//method that can be called to send data
+	//pre: open serial port
+	//post: data sent to the other device
+	public void writeData(int data)
+	{
+		String s = Integer.toString(data);
+		try
+		{
+			output.write(s.getBytes());
+			output.write(CARRIAGE_RETURN_ASCII);
+			output.flush();
+		}
+		catch (Exception e)
+		{
+			logText = "Failed to write data. (" + e.toString() + ")";
+			window.txtLog.setForeground(Color.red);
+			window.txtLog.append(logText + "\n");
+		}
+	}
 }
--- JAVA/src/KeyBinding.java
+++ JAVA/src/KeyBinding.java
@@ -10,64 +10,64 @@ import javax.swing.JButton;
 import javax.swing.KeyStroke;
 
 public class KeyBinding {
-    GUI window = null;
+	GUI window = null;
 
-    private static int valueToSend;
-    private static char send = 'q';
+	private static int valueToSend;
+	private static char send = 'q';
 
-    public KeyBinding(GUI window)
-    {
-        this.window = window;
-    }
+	public KeyBinding(GUI window)
+	{
+		this.window = window;
+	}
 
-    public void bindKeys()
-    {
-        //set input maps so that the program can read key bindings
-        //putting something in the input map means to assign a key to an action name
-        //action name is associated with a method in the action map
-    	
-        window.btnSend.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(send), "accelerateLeft");
-        window.btnSend.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(Character.toUpperCase(send)), "accelerateLeft");
-        window.btnSend.getActionMap().put("accelerateLeft", sending);
+	public void bindKeys()
+	{
+		//set input maps so that the program can read key bindings
+		//putting something in the input map means to assign a key to an action name
+		//action name is associated with a method in the action map
 
-    }
+		window.btnSend.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(send), "accelerateLeft");
+		window.btnSend.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(Character.toUpperCase(send)), "accelerateLeft");
+		window.btnSend.getActionMap().put("accelerateLeft", sending);
 
-    public void toggleControls()
-    {
-        if (window.communicator.getConnected() == true)
-        {
-            window.btnSend.setEnabled(true);
-            window.txtbox1.setEnabled(true);
+	}
 
-            window.btnDisconnect.setEnabled(true);
-            window.btnConnect.setEnabled(false);
-            window.cboxPorts.setEnabled(false);
-        }
-        else
-        {
-            window.btnSend.setEnabled(false);
-            window.txtbox1.setEnabled(false);
+	public void toggleControls()
+	{
+		if (window.communicator.getConnected() == true)
+		{
+			window.btnSend.setEnabled(true);
+			window.txtbox1.setEnabled(true);
 
-            window.btnDisconnect.setEnabled(false);
-            window.btnConnect.setEnabled(true);
-            window.cboxPorts.setEnabled(true);
-        }
-    }
+			window.btnDisconnect.setEnabled(true);
+			window.btnConnect.setEnabled(false);
+			window.cboxPorts.setEnabled(false);
+		}
+		else
+		{
+			window.btnSend.setEnabled(false);
+			window.txtbox1.setEnabled(false);
 
-    //defining the action
-    Action sending = new AbstractAction()
-    {
-        public void actionPerformed(ActionEvent evt)
-        {
-        		getTextToSend();
-        }
-    };
+			window.btnDisconnect.setEnabled(false);
+			window.btnConnect.setEnabled(true);
+			window.cboxPorts.setEnabled(true);
+		}
+	}
+
+	//defining the action
+	Action sending = new AbstractAction()
+	{
+		public void actionPerformed(ActionEvent evt)
+		{
+			getTextToSend();
+		}
+	};
 
 
-    final public int getTextToSend()
-    {
-    		valueToSend = Integer.parseInt(window.txtbox1.getText()); 
-        return valueToSend;
-    }
+	final public int getTextToSend()
+	{
+		valueToSend = Integer.parseInt(window.txtbox1.getText()); 
+		return valueToSend;
+	}
 
 }
